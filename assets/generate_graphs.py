eb729d6c1f0a7048c2afc43b4185831b414de18d
Luca Azalim <lucaazalim@gmail.com>
2024-11-04 19:16:29 -0300

    Organizing assets.

--- assets/generate_graphs.py
+++ assets/generate_graphs.py
@@ -1,16 +1,16 @@
 import networkx as nx
 
-# Grafo ciclo
+# Generates a cycle graph, which is always Eulerian
 def generate_eulerian_graph(n: int) -> nx.Graph:
     n -= 1
     graph = nx.Graph()
-    graph.add_nodes_from(range(n))
+    graph.add_nodes_from(range(n + 1))
     for j in range(n):
         graph.add_edge(j, j + 1)
     graph.add_edge(n, 0)
     return graph
 
-# Grafo linear
+# Generates a linear graph, which is always semi-Eulerian
 def generate_semieulerian_graph(n: int) -> nx.Graph:
     graph = nx.Graph()
     graph.add_nodes_from(range(n))
@@ -18,15 +18,14 @@ def generate_semieulerian_graph(n: int) -> nx.Graph:
         graph.add_edge(j, j + 1)
     return graph
 
-# Grafo linear com uma aresta adicional: {1, 3}
+# Generates a linear graph with an additional edge, making it non-Eulerian
 def generate_non_eulerian_graph(n: int) -> nx.Graph:
-    graph = nx.Graph()
-    graph.add_nodes_from(range(n))
-    for j in range(n - 1):
-        graph.add_edge(j, j + 1)
-    graph.add_edge(1, 3)
+    graph = generate_semieulerian_graph(n)
+    if n > 3:
+        graph.add_edge(1, 3)  # Adding an edge to make the graph non-Eulerian
     return graph
 
+# Writes the graph to a file
 def write_graph(graph: nx.Graph, filename: str):
     nx.write_edgelist(graph, filename, data=False)
 
@@ -35,17 +34,17 @@ N = 10
 for i in range(4):
     N *= 10
 
-    # Generate and write Eulerian graph
+    # Generates and writes Eulerian graph
     G = generate_eulerian_graph(N)
     assert nx.is_eulerian(G) and not nx.is_semieulerian(G)
     write_graph(G, f'eulerian_{N}_graph.txt')
 
-    # Generate and write Semi-Eulerian graph
+    # Generates and writes semi-Eulerian graph
     G = generate_semieulerian_graph(N)
     assert not nx.is_eulerian(G) and nx.is_semieulerian(G)
     write_graph(G, f'semi_eulerian_{N}_graph.txt')
 
-    # Generate and write Non-Eulerian graph
+    # Generates and writes non-Eulerian graph
     G = generate_non_eulerian_graph(N)
     assert not nx.is_eulerian(G) and not nx.is_semieulerian(G)
     write_graph(G, f'non_eulerian_{N}_graph.txt')
